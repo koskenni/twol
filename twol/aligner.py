60457033cdea8fb259a521bc838a3bd9111b8553
Kimmo Koskenniemi <koskenni@gmail.com>
2020-02-12 16:41:27 +0200

    preparing for an alternative multiple alignment

--- twol/aligner.py
+++ twol/aligner.py
@@ -8,24 +8,50 @@ Copyright 2017-2019, Kimmo Koskenniemi
 This is free software according to GNU GPL 3 license.
 """
 
+import hfst
+
+def align_two_words(in_word, out_word, aligner_fst, zero, number):
+    w1 = hfst.fst(in_word)
+    w1.insert_freely((zero,zero))
+    w1.minimize()
+    ###print(w1)
+
+    w2 = hfst.fst(out_word)
+    w2.insert_freely((zero,zero))
+    w2.minimize()
+    ###print(w2)
+
+    w3 = hfst.HfstTransducer(w1)
+    w3.compose(aligner_fst)
+    w3.compose(w2)
+    ###print(w1)
+
+    w3.n_best(number)
+    w3.minimize()
+    ###print(w3)
+
+    paths_str = w3.extract_paths(output='text')
+    return paths_str
+
+
 def main():
 
-    version = "2020-02-01"
+    version = "2020-02-12"
     
     import argparse
     arpar = argparse.ArgumentParser(
         "twol-aligner",
-        description="Aligns pairs of words separated by a colon. See"\
-        " https://pytwolc.readthedocs.io/en/latest/alignment.html"\
-        " for detailed instructions. Version {}".format(version))
+        description="""Aligns pairs of words separated by a
+        colon. See https://pytwolc.readthedocs.io/en/latest/alignment.html
+        for detailed instructions. Version {}""".format(version))
     arpar.add_argument(
         "metrics",
-        help="FST computed with twol-metric from an alphabet file."\
+        help="FST computed with twol-metric from an alphabet file."
         " The FST contains weights for phoneme correspondences.")
     arpar.add_argument(
         "-d", "--delimiter",
-        help="Separates the two cognates, default is ':'",
-        default=":")
+        help="Separates the two cognates, default is ' '",
+        default=" ")
     arpar.add_argument(
         "-l", "--layout",
         choices=["vertical","list","horizontal"],
@@ -33,15 +59,15 @@ def main():
         default="vertical")
     arpar.add_argument(
         "-c", "--comment-separator",
-        help="Comment separator. Comments in input"\
-        " after this character are just"\
-        " copied to output. Input words are then also copied to"\
-        " the end of comments. Default separator is '' i.e. no comments."\
-        " Comments come to the output only in horizontal layout.",
+        help="""Comment separator. Comments in input after this
+        character are just copied to output. Input words are then
+        also copied to the end of comments. Default separator is ''
+        i.e. no comments.  Comments come to the output only in
+        horizontal layout.""",
         default="")
     arpar.add_argument(
         "-w", "--weights",
-        help="print also the weight of each alignment."\
+        help="print also the weight of each alignment."
         " Default is not to print."
         " Works only if a comment separator is also set.",
         action="store_true")
@@ -51,49 +77,34 @@ def main():
         type=int, default=1)
     arpar.add_argument(
         "-v", "--verbosity",
-        help="Level of diagnostic information to be printed. Default is 0",
+        help="Level of diagnostic information to be printed. "
+        "Default is 0",
         type=int, default=0)
 
     args = arpar.parse_args()
 
-    import hfst
     algfile = hfst.HfstInputStream(args.metrics)
-    align = algfile.read()
+    aligner_fst = algfile.read()
 
     separator = args.delimiter
     import sys
     for line in sys.stdin:
         if args.comment_separator:
-            pair, comm, comments = line.strip().partition(args.comment_separator)
+            pair, comm, comments = \
+                line.strip().partition(args.comment_separator)
         else:
             pair, comm, comments = line.strip(), "", ""
         if args.verbosity > 0:
             print(pair, args.comment_separator, comm)
-        f1, sep, f2 =  pair.strip().partition(args.delimiter)
-        if not f2:
-            f2 = f1
+        in_word, sep, out_word =  pair.strip().partition(args.delimiter)
+        if not out_word:
+            out_word = in_word
 
-        w1 = hfst.fst(f1)
-        w1.insert_freely(("Ø","Ø"))
-        w1.minimize()
-    #    print(w1)
+        paths_str = align_two_words(in_word, out_word,
+                                    aligner_fst,
+                                    "Ø",
+                                    args.number)
 
-        w2 = hfst.fst(f2)
-        w2.insert_freely(("Ø","Ø"))
-        w2.minimize()
-    #    print(w2)
-
-        w3 = hfst.HfstTransducer(w1)
-        w3.compose(align)
-        w3.compose(w2)
-    #    print(w1)
-
-        w3.n_best(args.number)
-        w3.minimize()
-
-    #    print(w3)
-
-        paths_str = w3.extract_paths(output='text')
         paths_lst = paths_str.split(sep="\n")
         # print(paths_lst) ###
         for path in paths_lst:
@@ -131,9 +142,9 @@ def main():
                     weight = ""
                 if args.comment_separator:
                     if comments:
-                        comments = " " + comments + " | " + f1 + " " + f2
+                        comments = " " + comments + " | " + in_word + " " + out_word
                     else:
-                        comments = " " + f1 + " " + f2
+                        comments = " " + in_word + " " + out_word
                     algh = algh.ljust(30) + args.comment_separator + weight + comments
                 
                 print(algh)
