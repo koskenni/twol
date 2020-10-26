3cd5b082b5a66bbcf9800b7e54adf19fdd740f51
Kimmo Koskenniemi <koskenni@gmail.com>
2020-10-26 17:44:02 +0200

    rewritten multialign

--- twol/multialign.py
+++ twol/multialign.py
@@ -15,21 +15,28 @@ import twol.fs as fs
 import twol.cfg as cfg
 import twol.twbt as twbt
 import twol.alphabet as alphabet
-#import twol.metric as metric
-
-aligner_fst = hfst.HfstTransducer()
 
 
 def init(alphabet_file_name, all_zero_weight=1):
-    global aligner_fst
+    """Initializes multialign by initializing the alphabet module
+alphabet_file_name -- an alphabet definition as described in https://pytwolc.readthedocs.io/en/latest/alignment.html#alphabet
+
+all_zero_weight -- penalty for an intermediate morphophoneme of only zeros (which will get a non-zero component in the final morphophoneme)"""
     alphabet.read_alphabet(alphabet_file_name)
-    #aligner_fst = metric.alignment_fst()
-    if cfg.verbosity >= 20:
-        twbt.ppfst(aligner_fst)
     cfg.all_zero_weight = all_zero_weight
     return
 
+
 def print_result(aligned_result, comments, weights, layout="horizontal"):
+    """Prints the result of the alignment in one of the three formats
+
+aligned_result -- tuple of the weight and a list of aligned words where each aligned word is a list of 
+
+comments -- possible comments which will be passed over
+
+weights -- whether to print also the overall weight of this alignment
+
+layout -- one of "horizontal" (a sequence of morphophonemes on a single line), "vertical" (each zero-filled word on a line of its own) or "list" (all zero-filled words on a single line)"""
 
     weight, aligned_words_lst = aligned_result
     if cfg.verbosity >= 10:
@@ -62,7 +69,16 @@ def print_result(aligned_result, comments, weights, layout="horizontal"):
         print(" ".join(aligned_words_lst))
     return
 
-def accum_input_labels(fst, separator="", feat_weight_factor=5):
+
+def accum_input_labels(fst, separator=""):
+    """Encode, weight and prune a transducer
+
+fst -- transducer to be processed, input labels are strings of alphabet symbols and output labels are single alphabet symbols
+
+separator -- null string or a symbol not part of the alphabet
+
+Returns a transducer where input labels of thrasitions are concatenations of the input label and the output label of the original transition, the weights are according to the weights of the resulting morphophonemes and all transitions with invalid morphophoneme labels are discarded.
+"""
     if cfg.verbosity >= 10:
         print("to be accumulated:\n", fst)
     bfst = hfst.HfstBasicTransducer(fst)
@@ -77,34 +93,43 @@ def accum_input_labels(fst, separator="", feat_weight_factor=5):
             insym = arc.get_input_symbol()
             outsym = arc.get_output_symbol()
             weight = arc.get_weight()
-            res_insym = insym + separator + outsym
+            new_insym = insym + separator + outsym
             if cfg.verbosity >= 25:
                 print("arc", state, tostate, insym, outsym, weight)
-            if not alphabet.mphon_is_valid(res_insym):
+            if not alphabet.mphon_is_valid(new_insym):
                 continue
-            feat_weight = alphabet.mphon_weight(res_insym)
-            ###weight = weight + feat_weight_factor * feat_weight
-            weight = feat_weight
+            new_weight = alphabet.mphon_weight(new_insym)
             result_arc = hfst.HfstBasicTransition(tostate,
-                                                  res_insym,
-                                                  outsym,
-                                                  weight)
+                                                  new_insym,
+                                                  new_insym,
+                                                  new_weight)
             result_bfst.add_transition(state, result_arc)
             if cfg.verbosity >= 25:
                 print("after addition of transition:\n", result_bfst)
     result_fst = hfst.HfstTransducer(result_bfst)
     result_fst.minimize()
     if cfg.verbosity >= 10:
-        print("accumulated fst;\n", result_fst)
+        print("accumulated fst:\n", result_fst)
     return result_fst
 
-def list_of_aligned_words(sym_lst):
-    if not sym_lst:
+
+def list_of_aligned_words(mphon_lst):
+    """Converts a list of morphophonemes into a list of aligned words
+
+mphon_lst -- list of same length morphophonemes, e.g.
+    ["lll", "ooo", "vvv", "ieØ"]
+
+Returns a list of words constructed out of the 1st, 2nd ... alphabetic
+symbols of the morphophonemes, e.g.  ["lll", "ooo", "vvv", "ieØ"] -->
+["lovi", "love", "lovØ"]
+
+    """
+    if not mphon_lst:
         return []
-    lgth = grapheme.length(sym_lst[0])
+    lgth = grapheme.length(mphon_lst[0])
     res = []
     for i in range(lgth):
-        syms = [grapheme.slice(itm,start=i, end=i+1) for itm in sym_lst]
+        syms = [grapheme.slice(itm,start=i, end=i+1) for itm in mphon_lst]
         res.append("".join(syms))
     return res
 
@@ -119,13 +144,16 @@ def print_best(fst, num):
         alg_syms_str = " ".join(alg_syms_lst)
         print(weight, alg_syms_lst)
 
+
 def prefer_final_zeros(raw_paths):
     """Select the symbol pair sequence where the zeros are near the end
-    sym_lst_lst -- a list of results, each consisting of a list
+
+sym_lst_lst -- a list of results, each consisting of a list
     of symbols (already selected according to other criteria)
-    Returns a sequence of (single) symbols where the zeros occur near
-    the end.  This normalizes gemination and lengthening so that the
-    latter component is the one which alternates with a zero.
+
+Returns a sequence of (single) symbols where the zeros occur near the
+end.  This normalizes gemination and lengthening so that the latter
+component is the one which alternates with a zero.
     """
     if cfg.verbosity >= 20:
         print("prefer_final_zeros(raw_paths):", raw_paths)
@@ -150,106 +178,135 @@ def prefer_final_zeros(raw_paths):
         print("result_path_lst:", result_path_lst)
     return result_path_lst
 
-def align_words(word_lst,
-                zero="Ø",
-                max_zeros=1, best_count=10):
-    global aligner_fst
 
-    zero_fst = fs.string_to_fsa(zero)
-    word_len_lst = [grapheme.length(x) for x in word_lst]
-    word_len_max = max(word_len_lst)
-    if cfg.verbosity >= 10:
-        print("word_lst:", word_lst)
-        print("word_len_lst:", word_len_lst)
-        print("word_len_max:", word_len_max)
-    word_fst_lst = [fs.string_to_fsa(word) for word in word_lst]
-    zeros_fst = zero_fst.copy()
-    zeros_fst.repeat_n_to_k(word_len_max - word_len_lst[0],
-                            word_len_max - word_len_lst[0] + max_zeros)
-    zeros_fst.minimize()
-    if cfg.verbosity >= 10:
-        print("first zeros_fst:\n", zeros_fst)
-    fst = word_fst_lst[0].copy()
+def word_to_fsa_with_zeros(word, target_len, zero="Ø"):
+    """Insert zeros freeley to make the word into a fst of target_lenth words
+
+word -- a string
+
+target_len -- length of the strings accepted by the result fsa
+
+Returns a fsa that accepts all strings of length target_length where some zeros have been added to make the word be target_len long.
+"""
+    word_len = grapheme.length(word)
+    zeros_fst = fs.string_to_fsa(zero)
+    zeros_fst.repeat_n(target_len - word_len)
+    fst = fs.string_to_fsa(word)
     fst.shuffle(zeros_fst)
     fst.minimize()
-    i = 1
+    return fst
+
+
+def align_words(word_lst, zero="Ø", extra_zeros=0, best_count=10):
+    """Aligns a list of words
+
+word_lst -- the list of words to be aligned
+
+zero -- the symbol inserted as a mark for deletion or epenthesis
+
+extra_zeros -- the maximun number of zeros to be added in the longest
+    words (the shorter may have more)
+
+best_count -- the maximum number of results to be returned (maybe less
+    if no feasible results are found)
+
+Returns a list of tuples where each tuple consists of a weight and a
+list morphophonemes.
+    """
+    target_len = max([grapheme.length(x) for x in word_lst]) + extra_zeros
+    fst = word_to_fsa_with_zeros(word_lst[0], target_len, zero)
     if cfg.verbosity >= 10:
-        twbt.ppfst(fst, title=word_lst[0])
-    fact = 10 * len(word_lst) if i == len(word_lst) else 0
-    for word, word_len, word_fst in zip(word_lst[1:],
-                                        word_len_lst[1:], word_fst_lst[1:]):
-        fst.compose(aligner_fst)
-        ###fst.n_best(best_count + 20) ####
-        fst.minimize()
-        fact = 5
-        fst = accum_input_labels(fst, feat_weight_factor=fact) #########
+        print("first fst with shuffled zeros:\n", fst)
+    i = 1
+    for word in word_lst[1:]:
+        word_fsa = word_to_fsa_with_zeros(word, target_len, zero)
+        fst.cross_product(word_fsa)
+        fst = accum_input_labels(fst)
         if cfg.verbosity >= 10:
-            print("fst composed with aligner:\n", fst)
-        zeros_fst = zero_fst.copy()
-        zeros_fst.repeat_n_to_k(word_len_max - word_len,
-                                word_len_max - word_len + max_zeros)
-        zeros_fst.minimize()
-        if cfg.verbosity >= 10:
-            print("zeros_fst:\n", zeros_fst)
-        word_fst.shuffle(zeros_fst)
-        word_fst.minimize()
-        if cfg.verbosity >= 10:
-            print("shuffled word_fst:\n", word_fst)
-        i += 1
-        fst.compose(word_fst)
-        fst.n_best(best_count + 50) ####
-        fst.minimize()
-        if cfg.verbosity >= 10:
-            print("composed fst:\n", fst)
-        fst.minimize()
-        ##fst = accum_input_labels(fst, feat_weight_factor=fact)
-        if cfg.verbosity >= 1:
-            print_best(fst, 1) #########
-        if cfg.verbosity >= 10:
-            print(word + "\n", fst)
-    
+            print("fst accumulated with", word, ":\n", fst)
+
     fst.n_best(best_count)
     fst.minimize()
     if cfg.verbosity >= 10:
         print("aligned result:\n", fst)
-    raw_paths = fst.extract_paths(output='raw')
-    if not raw_paths:
-        return []
+    weight_path_lst = fst.extract_paths(output='raw')
+    weight_mphon_lst_lst = []
+    for weight, path_lst in weight_path_lst:
+        mphon_lst = [insym for insym, outsym in path_lst]
+        weight_mphon_lst_lst.append((weight, mphon_lst))
     if cfg.verbosity >= 10:
-        print("raw_paths:", raw_paths)
-    preferred_raw_path_lst = prefer_final_zeros(raw_paths)
-    return preferred_raw_path_lst
+        print("weight_mphon_lst_lst:", weight_mphon_lst_lst)
+    return weight_mphon_lst_lst
+
+
+def adjustment(mphon_lst):
+    """Computes an context based adjustment of a result
+
+mphon_lst -- a list of morphophonemes from the result of align_words
+
+Returns a number to be added to the weight.
+    """
+    adj = 10 * len(mphon_lst)
+    phon_set_lst = [set(mphon)  for mphon in mphon_lst]
+    if phon_set_lst[-1] <= alphabet.vowel_set | set("Ø"):
+        adj -= 20
+    for this_set, next_set in zip(phon_set_lst[:-1], phon_set_lst[1:]):
+        if len(this_set) == 1 and next_set == this_set | set("Ø"):
+            adj -= 20
+    return adj
 
 def multialign(word_lst,
                zero="Ø",
                max_zeros=1, best_count=1):
+    """Aligns a list of words according to similarity of their phonemes
+
+word_lst -- a list of words (or morphs) to be aligned
+
+zero -- the symbol that will mark deletions or epentheses
+
+max_zeros -- maximum number of zeros to be inserted into the longest
+    word
+
+best_count -- number of results to returned
+
+Returns a list of results each of which is a tuple of a weight and a
+list of words aligned by inserting zeros in an optimal way.
+
+    """
     if len(word_lst) < 1:
-        return []
+        return []                 # nothing to align
     elif len(word_lst) == 1:
-        return ((0,(word_lst),),)
-    
+        return ((0,(word_lst),),) # only one word to align
+
+    # process only the subset of unique words starting from the longest
     unique_words_lst = sorted(list(set(word_lst)),
                               key= lambda word: -len(word))
-    raw_paths = align_words(unique_words_lst,
-                            zero=zero,
-                            max_zeros=max_zeros,
-                            best_count=best_count+9)
     aligned_results_lst = []
-    for raw_path in raw_paths[:best_count]:
-        weight, pairs_tuple = raw_path
-        alg_syms_lst = [alg_syms for alg_syms, out_sym in pairs_tuple]
-        #** calculate here a penalty for word internal deletions **
-        if cfg.verbosity >= 10:
-            print("alg_syms_lst", alg_syms_lst)
-        aligned_words_lst = list_of_aligned_words(alg_syms_lst)
-        d = {}
-        for uniqw, aligw in zip(unique_words_lst, aligned_words_lst):
-            d[uniqw] = aligw
-        aligned_orig_words_lst = [d[word] for word in word_lst]
-        aligned_results_lst.append((weight, aligned_orig_words_lst))
-    if cfg.verbosity >= 20:
+    for i in range(max_zeros + 1):
+        result_lst = align_words(unique_words_lst,
+                                 zero=zero,
+                                 extra_zeros=i,
+                                 best_count=best_count+9)
+    
+        for weight, alg_syms_lst in result_lst[:best_count]:
+            if weight >= 1000:
+                continue
+            if cfg.verbosity >= 10:
+                print("alg_syms_lst", alg_syms_lst)
+            weight += adjustment(alg_syms_lst)
+        
+            aligned_words_lst = list_of_aligned_words(alg_syms_lst)
+
+            # apply the alignment to the original word list with possible duplicates
+            d = {}
+            for uniqw, aligw in zip(unique_words_lst, aligned_words_lst):
+                d[uniqw] = aligw
+            aligned_orig_words_lst = [d[word] for word in word_lst]
+            aligned_results_lst.append((weight, aligned_orig_words_lst))
+    
+    if cfg.verbosity >= 10:
         print("aligned_results_lst:", aligned_results_lst)
-    return aligned_results_lst
+    return sorted(aligned_results_lst, key = lambda r: r[0])[:best_count]
         
 def main():
 
@@ -306,7 +363,7 @@ def main():
     if args.verbosity:
         cfg.verbosity = args.verbosity
 
-    init(args.alphabet)
+    init(args.alphabet, all_zero_weight=1000)
 
     separator = args.delimiter
     import sys
